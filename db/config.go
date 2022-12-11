64ddc57fffc44679f6a6df373d06a29171340b29
VinayakBagaria <workvinayak95@gmail.com>
2022-12-11 23:46:34 +0530

    Add user tests for repository

--- db/config.go
+++ db/config.go
@@ -1,7 +1,6 @@
 package db
 
 import (
-	"fmt"
 	"log"
 	"os"
 	"strconv"
@@ -24,7 +23,7 @@ type config struct {
 func NewConfig() Config {
 	var cfg config
 	cfg.dbUser = os.Getenv("DATABASE_USER")
-	cfg.dbPass = os.Getenv("DATABASE_PASSWORD")
+	cfg.dbPass = os.Getenv("DATABASE_PASS")
 	cfg.dbHost = os.Getenv("DATABASE_HOST")
 	var err error
 	cfg.dbPort, err = strconv.Atoi(os.Getenv("DATABASE_PORT"))
@@ -32,7 +31,8 @@ func NewConfig() Config {
 		log.Fatalln("Error on load env var: ", err.Error())
 	}
 	cfg.dbName = os.Getenv("DATABASE_NAME")
-	cfg.dsn = fmt.Sprintf("mongodb://%s:%s@%s:%d/%s", cfg.dbUser, cfg.dbPass, cfg.dbHost, cfg.dbPort, cfg.dbName)
+	// cfg.dsn = fmt.Sprintf("mongodb://%s:%s@%s:%d/%s?authMechanism=SCRAM-SHA-1&authSource=%s", cfg.dbUser, cfg.dbPass, cfg.dbHost, cfg.dbPort, "", cfg.dbName)
+	cfg.dsn = "mongodb://user:password@localhost:27017/?authSource=microservices"
 	return &cfg
 }
 
--- go.mod
+++ go.mod
@@ -3,8 +3,12 @@ module github.com/VinayakBagaria/auth-micro-service
 go 1.19
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.8.1 // indirect
 	google.golang.org/protobuf v1.26.0 // indirect
 	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
