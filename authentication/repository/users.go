bbee621ecb68be3331fc0ce480a44a1af5785db9
VinayakBagaria <workvinayak95@gmail.com>
2022-12-11 21:59:05 +0530

    Add user model definition for mongo, user repository for crud operations

---
+++ authentication/repository/users.go
@@ -0,0 +1,57 @@
+package repository
+
+import (
+	"github.com/VinayakBagaria/auth-micro-service/authentication/models"
+	"github.com/VinayakBagaria/auth-micro-service/db"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+const usersCollection = "users"
+
+type UsersRepository interface {
+	Save(user *models.User) error
+	GetById(id string) (*models.User, error)
+	GetByEmail(email string) (*models.User, error)
+	GetAll() ([]*models.User, error)
+	Update(user *models.User) error
+	Delete(id string) error
+}
+
+type usersRepository struct {
+	c *mgo.Collection
+}
+
+func NewUsersRepository(conn db.Connection) UsersRepository {
+	return &usersRepository{conn.DB().C(usersCollection)}
+}
+
+func (r *usersRepository) Save(user *models.User) error {
+	return r.c.Insert(user)
+}
+
+func (r *usersRepository) GetById(id string) (*models.User, error) {
+	var user *models.User
+	err := r.c.FindId(bson.ObjectIdHex(id)).One(&user)
+	return user, err
+}
+
+func (r *usersRepository) GetByEmail(email string) (*models.User, error) {
+	var user *models.User
+	err := r.c.FindId(bson.M{"email": email}).One(&user)
+	return user, err
+}
+
+func (r *usersRepository) GetAll() ([]*models.User, error) {
+	var users []*models.User
+	err := r.c.FindId(bson.M{}).All(&users)
+	return users, err
+}
+
+func (r *usersRepository) Update(user *models.User) error {
+	return r.c.UpdateId(user.Id, user)
+}
+
+func (r *usersRepository) Delete(id string) error {
+	return r.c.RemoveId(bson.ObjectIdHex(id))
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/VinayakBagaria/auth-micro-service
 go 1.19
 
 require (
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
 	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
 )
