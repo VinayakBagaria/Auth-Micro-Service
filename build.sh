e0c1524cd40198b6a5e7d944b8ca7f409ad18b99
VinayakBagaria <workvinayak95@gmail.com>
2022-12-12 09:52:59 +0530

    Add api service entry point

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@
 
 go clean --cache && go test -v -cover github.com/VinayakBagaria/auth-micro-service/...
 go build -o authentication/authsvc authentication/main.go
+go build -o api/apisvc api/main.go
