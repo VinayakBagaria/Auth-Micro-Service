5a9bb0d7da34943e98464929855d283f29a6555f
VinayakBagaria <workvinayak95@gmail.com>
2022-12-12 08:15:21 +0530

    Add code to start grpc service and register repo/service

--- authentication/main.go
+++ authentication/main.go
@@ -2,18 +2,26 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"net"
 
+	"github.com/VinayakBagaria/auth-micro-service/authentication/repository"
+	"github.com/VinayakBagaria/auth-micro-service/authentication/service"
 	"github.com/VinayakBagaria/auth-micro-service/db"
+	"github.com/VinayakBagaria/auth-micro-service/pb"
 	"github.com/joho/godotenv"
+	"google.golang.org/grpc"
 )
 
 var (
 	local bool
+	port  int
 )
 
 func init() {
 	flag.BoolVar(&local, "local", true, "run service local")
+	flag.IntVar(&port, "port", 9001, "run authentication service port")
 	flag.Parse()
 }
 
@@ -31,4 +39,17 @@ func main() {
 		log.Panicln(err)
 	}
 	defer conn.Close()
+
+	usersRepository := repository.NewUsersRepository(conn)
+	authService := service.NewAuthService(usersRepository)
+
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterAuthServiceServer(grpcServer, authService)
+	log.Printf("Authentication service running on [::]:%d\n", port)
+	grpcServer.Serve(lis)
 }
--- go.mod
+++ go.mod
@@ -8,7 +8,12 @@ require (
 	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/testify v1.8.1 // indirect
-	google.golang.org/protobuf v1.26.0 // indirect
+	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
+	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
+	golang.org/x/text v0.4.0 // indirect
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
+	google.golang.org/grpc v1.51.0 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
